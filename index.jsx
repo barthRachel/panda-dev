070a46923d03a9778c91f362e6e019dd8c0354a3
Rachel Barthélery <43150113+barthRachel@users.noreply.github.com>
2022-12-26 01:35:28 +0100

    Modifications deploy

--- index.jsx
+++ index.jsx
@@ -1,38 +1,36 @@
-import './header.css';
-import React, {useState} from 'react';
-import { Link } from 'react-router-dom';
-import logo from '../../images/logotest.PNG';
-import { FaHome, FaQuoteLeft, FaBook, FaTv } from 'react-icons/fa';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function Header() {
+import './index.css'
+import Home from './pages/Home';
+import Quote from './pages/Quote';
+import Book from './pages/Book';
+import Movie from './pages/Movie';
+import Error from './components/Error';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-  var [before, setBefore] = useState('Home');
 
-  const changePage = event => {
-    console.log(before)
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
 
-    let getBefore = document.getElementById(before);
-    getBefore.classList.remove('navigationOn');
+root.render(
+  <Router>
+    <Header/>
+    
+    <Routes>
+      <Route exact path='/panda-dev' element={<Home />} />
 
-    event.currentTarget.classList.add('navigationOn');
-    var linkClicked = event.target.dataset.id;
-    setBefore(linkClicked);
-  }
+      <Route path='/quote' element={<Quote />} />
 
-  return (
-    <div className='headerDiv'>
-      <p>
-        <img src={logo} alt="Logo PandaDev" className='logoNav'/>
-      </p>
+      <Route path='/book' element={<Book />} />
 
-      <nav className='navHeader'>
-        <Link to="/home" id='Home' data-id="Home" className='navigationLink navigationOn' onClick={changePage}> <FaHome className='iconHeader'/> Accueil</Link>
-        <Link to="/quote" id='Quote' data-id="Quote" className='navigationLink' onClick={changePage}> <FaQuoteLeft className='iconHeader'/> Citations</Link>
-        <Link to="/book" id='Book' data-id="Book" className='navigationLink' onClick={changePage}><FaBook className='iconHeader'/> Livres</Link>
-        <Link to="/movie" id='Movie' data-id="Movie" className='navigationLink' onClick={changePage}><FaTv className='iconHeader'/> Films</Link>
-      </nav>
-    </div>
-  );
-}
+      <Route path='/movie' element={<Movie />} />
 
-export default Header;
+      <Route path='/*' to="/error_404" element={<Error/>} />
+    </Routes>
+    
+    <Footer />
+  </Router>
+);
